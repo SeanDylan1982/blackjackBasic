49c0516fe3746dafdd8dab29b02a1f4b99a2230d
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-05-29 22:45:23 +0200

    Deploy application to Netlify

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = window.env.REACT_APP_SUPABASE_URL
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = window.env.REACT_APP_PUBLIC_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
