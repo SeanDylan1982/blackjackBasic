b1fcaddd3821b3862a20d6dc0e522346e89ecf06
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-05-29 22:22:47 +0200

    Deploy application to Netlify

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'process.env.SUPABASE_URL'
-const supabaseAnonKey = 'process.env.PUBLIC_SUPABASE_KEY'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.PUBLIC_SUPABASE_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables')
