2ab6d6a0765cca1beb2e61cd27b64e89582291c8
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-05-29 21:31:06 +0200

    Deploy application to Netlify

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'postgresql://postgres:LL0921jj@db.pdftvqjqwrnwgacmtecc.supabase.co:5432/postgres'
+const supabaseUrl = 'pdftvqjqwrnwgacmtecc.supabase.co:5432/postgres'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkZnR2cWpxd3Jud2dhY210ZWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MzQ2OTAsImV4cCI6MjA2NDExMDY5MH0.OkJjvTFSF1iDbG5tdSiFw34CnPfskZOAcJWtztYQicQ'
 
 if (!supabaseUrl || !supabaseAnonKey) {
