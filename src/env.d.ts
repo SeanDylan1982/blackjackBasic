cff8a05d2b4941adf2151184c2c75d3ab2b31792
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-05-29 21:50:53 +0200

    Updated env.d.ts

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string;
+  readonly PUBLIC_SUPABASE_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
