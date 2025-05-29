616ed3bbe32680b55d77edb28cc5ed8cc2c3fd83
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-05-29 22:32:12 +0200

    Deploy application to Netlify

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly SUPABASE_URL: string;
-  readonly PUBLIC_SUPABASE_KEY: string;
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
